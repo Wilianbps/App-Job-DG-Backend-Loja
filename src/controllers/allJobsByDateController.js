ebb25e3f49e43b876a510e3c735734622789c883
Wilian Brito <contato.beitsoft@gmail.com>
2024-03-04 15:21:46 -0300

    first commit

--- src/controllers/allJobsByDateController.js
+++ src/controllers/allJobsByDateController.js
@@ -3,17 +3,21 @@ import allJobsByDateModels from "../models/allJobsByDateModels.js";
 async function getAllJobs(req, res) {
   const { startTime } = req.query;
 
-  console.log(startTime)
+  console.log(startTime);
+
+  console.log(startTime);
   try {
     const allJobsByDate = await allJobsByDateModels.getJobsByDate(startTime);
-     const transformPropsAllJobs = await allJobsByDate.recordsets[0].map((job) => ({
-      id: job.ID,
-      name: job.NOME,
-      startTime: job.DATA_HORA,
-      table: job.TABELA,
-      action: job.ACAO,
-      status: job.STATUS_JOB,
-    }));
+    const transformPropsAllJobs = await allJobsByDate.recordsets[0].map(
+      (job) => ({
+        id: job.ID,
+        name: job.NOME,
+        startTime: job.DATA_HORA,
+        table: job.TABELA,
+        action: job.ACAO,
+        status: job.STATUS_JOB,
+      })
+    );
 
     return res.status(200).json(transformPropsAllJobs);
   } catch (error) {
